5cef787f63c4f2a35ff79bde4e5903c104148ad5
lobingera <lobingera@gmail.com>
2016-05-21 10:40:06 +0200

    perlin in window, rendering to size

--- local_testing/gtk-compose.jl
+++ local_testing/gtk-compose.jl
@@ -1,11 +1,11 @@
 using Gtk
-using Cairo
 using Compose
+using Cairo
 
 function cm_in_context(cm)
 	n = length(cm)
-	r = rectangle(linspace(0,1-(1/n),n),0.1*ones(n),ones(n)*(1.1/(n)),0.8*ones(n));
-	c = compose(context(),r,fill(cm));
+	r = Compose.rectangle(linspace(0,1-(1/n),n),0.1*ones(n),ones(n)*(1.1/(n)),0.8*ones(n));
+	c = Compose.compose(context(),r,fill(cm));
 	c
 	end
 
@@ -25,6 +25,9 @@ function map_from_colormap(cm,z::Array{Float64,2})
      z1 = z .- minimum(z);
      z2 = round(Int,max(ceil(z1  * (length(cm) / maximum(z1))),1));
     
+     display(minimum(z2));
+     display(maximum(z2));
+     
      c = similar(z,UInt32);
 
      for i1 in eachindex(z2)
@@ -47,16 +50,13 @@ function cm_perlin_window(cm)
 
 	Gtk.draw(c) do widget
 		cr = Cairo.CairoContext(c.back)
-		Cairo.rectangle(cr,20.0,20.0,180.0,180.0)
+		r = Gtk.allocation(c)
+		iwidth = Float64(r.width)-20.0;
+		iheight = Float64(r.height)-20.0;
+		Cairo.rectangle(cr,20.0,20.0,iwidth,iheight)
 
-    #imgmatrix = Cairo.CairoMatrix(imgsurf.width/w, 0, 0, imgsurf.height/h, 0, 0)
-    #Cairo.set_matrix(imgpatt, imgmatrix)
-
-    
-    # Translate to draw position and draw
-    #Cairo.translate(img.ctx, x, y)
-    #Cairo.set_source(img.ctx, imgpatt)
-    #Cairo.fill(img.ctx)
+    	imgmatrix = Cairo.CairoMatrix(120.0/iwidth, 0, 0, 120.0/iheight, 0, 0)
+    	Cairo.set_matrix(imgpatt, imgmatrix)
 
 	    Cairo.set_source(cr, imgpatt)
     	Cairo.fill(cr)
